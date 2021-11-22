5ee9fef5f8d7ffaef9a56aafcba6759fcd17205b
andrew4779 <andrewkimani02@gmail.com>
2021-11-22 21:29:44 +0300

    updated js folder

--- js/script.js
+++ js/script.js
@@ -85,7 +85,6 @@ $(document).ready(function(){
   
   $(document).ready(function(){
     $("form#form34A").submit(function(event){
-      // event.preventDefault();
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();
